6b6257d2f83a86ee5bfc636ba7f3356889949c0f
Rodrigo Régio de Araujo <rodrigo_1567@hotmail.com>
2021-01-28 19:36:39 -0300

    Criado perguntas no index
    Mostrando as perguntas disponiveis na página index.html (provavelmente isto será alterado) mas cheguei na aula 28

--- questoes/views.py
+++ questoes/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import PerguntaForm,AlternativaForm
 from .models import Pergunta,Alternativa
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    perguntas=Pergunta.objects.all()
+    return render(request, 'index.html', {'perguntas' : perguntas})
 
 def pergunta_inserir(request):
     form_pergunta=PerguntaForm(request.POST)
@@ -17,5 +18,16 @@ def pergunta_inserir(request):
             alternativa = form_alternativa.save(commit=False)
             alternativa.pergunta=pergunta
             alternativa.save()
+        redirect('index')
+    return render(request,'pergunta_form.html',{'form_pergunta':form_pergunta,'form_alternativas':form_alternativas})
+
+def pergunta_atualizar(request,id):
+    pergunta=Pergunta.objects.get(pk=id)
+    form_pergunta=PerguntaForm(request.POST or None, instance=pergunta)
+    form_alternativas=[AlternativaForm(request.POST or None, instance=alternativa, prefix=str(alternativa.id)) for alternativa in pergunta.alternativa.all()]
+    if(form_pergunta.is_valid() and all(form_alternativa.is_valid() for form_alternativa in form_alternativas)):
+        form_pergunta.save()
+        for form_alternativa in form_alternativas:
+            form_alternativa.save()
         redirect('index')
     return render(request,'pergunta_form.html',{'form_pergunta':form_pergunta,'form_alternativas':form_alternativas})
